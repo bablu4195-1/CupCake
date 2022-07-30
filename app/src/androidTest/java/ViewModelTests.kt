229c621880ac39e03e72c566ddfd525d997b82b6
Venkata Sai Teja Mukkapati <55186465+bablu4195-1@users.noreply.github.com>
2022-07-30 15:31:55 +0530

    iteration 1

--- app/src/androidTest/java/ViewModelTests.kt
+++ app/src/androidTest/java/ViewModelTests.kt
@@ -1,2 +1,14 @@
+import com.example.cupcake.model.OrderViewModel
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
 class ViewModelTests {
+
+    @Test
+    fun price_twelve_cupcakes() {
+        val viewModel = OrderViewModel()
+        viewModel.price.observeForever {}
+        viewModel.setQuantity(12)
+        assertEquals("$27.00", viewModel.price.value)
+    }
 }
